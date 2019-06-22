2384a1912f8f51c968f445565ff5d2b272b2c700
Lutfi <lutfidiann@gmail.com>
2019-06-22 09:57:15 +0700

    second commit

--- app/src/main/java/id/cashflow/android/MainActivity.kt
+++ app/src/main/java/id/cashflow/android/MainActivity.kt
@@ -1,6 +1,9 @@
 package id.cashflow.android
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import id.cashflow.android.util.Calculator
@@ -33,4 +36,25 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_main_setting,menu)
+        return true
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when(item?.itemId){
+            R.id.action_setting -> {
+                Toast.makeText(this@MainActivity, "setting",Toast.LENGTH_SHORT).show()
+                    true
+            }
+            R.id.action_logout ->{
+                Toast.makeText(this@MainActivity,"logout",Toast.LENGTH_SHORT).show()
+                    true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
