9a340b7c4beb417eb5eb4e4fa165bf8ab8339ab7
Lutfi <lutfidiann@gmail.com>
2019-06-30 20:31:58 +0700

    chalange 3

---
+++ app/src/main/java/id/cashflow/android/ui/LoginActivity.kt
@@ -0,0 +1,13 @@
+package id.cashflow.android.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import id.cashflow.android.R
+
+class LoginActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
---
+++ app/src/main/java/id/cashflow/android/ui/MainActivity.kt
@@ -0,0 +1,101 @@
+package id.cashflow.android.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.MenuInflater
+import android.widget.PopupMenu
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import id.cashflow.android.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initView()
+        initFragment()
+    }
+
+    private fun initFragment(){
+        val mainFragment = MainFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.flFragment, mainFragment)
+            .commit()
+    }
+
+    private fun initView(){
+        ivNotification.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Notification", Toast.LENGTH_SHORT).show()
+        }
+        ivSetting.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Setting", Toast.LENGTH_SHORT).show()
+        }
+
+        ivSetting.setOnClickListener { view ->
+            val popup = PopupMenu(this@MainActivity, view)
+            val inflater: MenuInflater = popup.menuInflater
+            inflater.inflate(R.menu.menu_main_setting, popup.menu)
+            popup.setOnMenuItemClickListener {
+                when (it.itemId) {
+                    R.id.action_setting -> {
+                        Toast.makeText(this@MainActivity, "setting", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    R.id.action_logout -> {
+                        onLogoutClick()
+                        true
+                    }
+                    else -> super.onOptionsItemSelected(it)
+                }
+            }
+            popup.show()
+        }
+    }
+
+    private fun onLogoutClick(){
+        startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+        finish()
+    }
+
+
+}
+
+//        println("Halo Bandung!")
+//
+//        val calc = Calculator()
+//        val resultSum :Int = calc.sum(x=5, y=5)
+//        println("result : ${resultSum}")
+//        val resultSub :Int = calc.sub(x=5, y=5)
+//        println("result : ${resultSub}")
+//        val resultTimes :Int = calc.times(x=5, y=5)
+//        println("result : ${resultTimes}")
+//        val resultdiv :Int = calc.div(x=5, y=5)
+//        println("result : ${resultdiv}")
+//
+//    }
+//
+//
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        val inflater: MenuInflater = menuInflater
+//        inflater.inflate(R.menu.menu_main_setting,menu)
+//        return true
+//
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+//        return when(item?.itemId){
+//            R.id.action_setting -> {
+//                Toast.makeText(this@MainActivity, "setting",Toast.LENGTH_SHORT).show()
+//                    true
+//            }
+//            R.id.action_logout ->{
+//                Toast.makeText(this@MainActivity,"logout",Toast.LENGTH_SHORT).show()
+//                    true
+//            }
+//            else -> super.onOptionsItemSelected(item)
+//        }
+//    }
+//}
